9a0dae8217f8fe6d277abedc13c658116f70cf24
Nizar Baihaqi <nizar.albaihaqi252@gmail.com>
2022-05-22 09:35:50 +0700

    Color Palette 🎨

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,14 @@ module.exports = {
       'barlow': ['Barlow', 'sans-serif'],
       'rubik': ['Rubik', 'sans-serif']
     },
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#f7b81c',
+        'action': '#ff8c00',
+        'init': '#FF2E2E',
+        'last': '#FCDD3C'
+      }
+    },
   },
   plugins: [],
 }
