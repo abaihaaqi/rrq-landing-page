0aae2269df3f22c010b495ed054a2070ef1fe5ec
Nizar Baihaqi <nizar.albaihaqi252@gmail.com>
2022-05-22 08:17:48 +0700

    Add Tailwindcss 🛠️

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    fontFamily: {
+      'barlow': ['Barlow', 'sans-serif'],
+      'rubik': ['Rubik', 'sans-serif']
+    },
+    extend: {},
+  },
+  plugins: [],
+}
