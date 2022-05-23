87d48d42f01c6ec140550af1ea4ce8ea115a1ac4
Nizar Baihaqi <nizar.albaihaqi252@gmail.com>
2022-05-23 11:52:15 +0700

    Create News Component 🛠️

---
+++ components/news-item.js
@@ -0,0 +1,18 @@
+import Image from 'next/image'
+
+export default function NewsItem({ newsImage, division, date, title }) {
+  return (
+    <div className='relative h-[280px]'>
+      <Image src={newsImage} layout='fill' objectFit='cover' />
+      <div className='absolute inset-0 bg-gradient-to-t from-black/70 via-black/20 to-black/0 flex flex-col justify-end text-white p-2'>
+        <small className='text-xs'>
+          <span className='text-primary'>{division}</span> - {date}
+        </small>
+        <p className='font-bold'>
+          {title}
+        </p>
+      </div>
+    </div>
+  )
+}
+  
