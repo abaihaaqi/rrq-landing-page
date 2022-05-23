9e603d1109028ec32d178bc6fb100e9908a54429
Nizar Baihaqi <nizar.albaihaqi252@gmail.com>
2022-05-23 11:53:38 +0700

    Create Division Component 🛠️

---
+++ components/division-item.js
@@ -0,0 +1,36 @@
+import Image from 'next/image'
+import { Desktop, DeviceMobile, GameController } from 'phosphor-react'
+
+export default function DivisionItem({ background, logo, device }) {
+  function deviceIs() {
+    if (device == 'pc') {
+      return <Desktop size={24} weight='thin' />
+    } else if (device == 'console') {
+      return <GameController size={24} weight='thin' />
+    } else {
+      return <DeviceMobile size={24} weight='thin' />
+    }
+  }
+
+  return (
+    <div className='relative h-[200px]'>
+      <Image src={background} layout='fill' objectFit='cover' />
+      <div className='absolute inset-0 bg-gradient-to-t from-black/70 via-black/40 to-black/0 flex flex-col justify-center text-white p-2'>
+        <div className='flex justify-center'>
+          <div className='w-1/3'>
+            <Image src={logo} />
+          </div>
+        </div>
+        <div className='flex justify-center'>
+          <button className='border border-last text-xs text-last px-2 py-1 rounded-sm'>
+            View Players
+          </button>
+        </div>
+      </div>
+      <div className='absolute right-2 top-2 bg-last opacity-60 rounded-sm'>
+        {deviceIs()}
+      </div>
+    </div>
+  )
+}
+  
