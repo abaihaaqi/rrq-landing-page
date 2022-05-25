05a2fa8426fa679becb925a90c1ec1b46ba39937
Nizar Baihaqi <nizar.albaihaqi252@gmail.com>
2022-05-25 08:00:28 +0700

    Create achievement item component 🛠️

---
+++ components/achievement-item.js
@@ -0,0 +1,60 @@
+import Image from 'next/image'
+import trophy1 from '../public/images/trophy-1.png'
+import trophy2 from '../public/images/trophy-2.png'
+import trophy3 from '../public/images/trophy-3.png'
+
+export default function AchievementItem({ logoGame, logoTournament, tournament, year, team, position }) {
+  function positionIs() {
+    if (position == 1) {
+      return (
+        <div className='col-span-2 my-1'>
+          <div className='flex justify-center'>
+            <Image src={trophy1} />
+          </div>
+          <p className='text-sm'>
+            1st Place
+          </p>
+        </div>
+      )
+    } else if (position == 2) {
+      return (
+        <div className='col-span-2 my-1'>
+          <div className='flex justify-center'>
+            <Image src={trophy2} />
+          </div>
+          <p className='text-sm'>
+            2nd Place
+          </p>
+        </div>
+      )
+    } else {
+      return (
+        <div className='col-span-2 my-1'>
+          <div className='flex justify-center'>
+            <Image src={trophy3} />
+          </div>
+          <p className='text-sm'>
+            3rd Place
+          </p>
+        </div>
+      )
+    }
+  }
+
+  return (
+    <div className='grid grid-cols-12 border-b border-black/40'>
+      <div className='bg-slate-200 flex justify-center items-center border-r-2 border-secondary'>
+        <Image src={logoGame} />
+      </div>
+      <div className='col-span-2 flex justify-center items-center'>
+        <Image src={logoTournament} />
+      </div>
+      <div className='col-span-7 flex flex-col justify-center'>
+        <h3 className='font-medium'>{tournament}</h3>
+        <p className='text-sm'>{year} - {team}</p>
+      </div>
+      {positionIs()}
+    </div>
+  )
+}
+  
