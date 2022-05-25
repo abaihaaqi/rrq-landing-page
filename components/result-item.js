a0beb2aeb2fc81588eb04a10739b5afefc40d13b
Nizar Baihaqi <nizar.albaihaqi252@gmail.com>
2022-05-25 08:00:06 +0700

    Create result item component 🛠️

---
+++ components/result-item.js
@@ -0,0 +1,40 @@
+import Image from 'next/image'
+
+export default function ResultItem({ logoGame, logoDefender, logoAttacker, match, defender, attacker, phase, win, defenderPoint, attackerPoint }) {
+  return (
+    <div className='grid grid-cols-12 border-b border-black/40'>
+      <div className='bg-slate-200 flex justify-center items-center border-r-2 border-secondary'>
+        <Image src={logoGame} />
+      </div>
+      <div className='col-span-7 flex flex-col justify-center py-2 pl-1'>
+        <h3 className='font-medium truncate'>{match}</h3>
+        <div className='truncate text-sm flex gap-1'>
+          <span className='w-4'>
+            <Image src={logoDefender} />
+          </span>
+          <span>
+            {defender}
+          </span>
+          <span className='font-light'>
+            VS
+          </span>
+          <span className='w-4'>
+            <Image src={logoAttacker} />
+          </span>
+          <span className='truncate'>
+            {attacker}
+          </span>
+        </div>
+      </div>
+      <div className='text-right col-span-4 flex flex-col justify-center'>
+        <div className='truncate text-sm'>
+          {phase}
+        </div>
+        <div className='text-sm'>
+          <span className='font-bold'>{win ? 'Win' : 'Lose'}</span> {defenderPoint} : {attackerPoint}
+        </div>
+      </div>
+    </div>
+  )
+}
+  
